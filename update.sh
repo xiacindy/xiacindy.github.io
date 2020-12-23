efed392027b9b3d2b675f6ec60cf78c16fd97aa2
wuben2602 <wuben2602@utexas.edu>
2020-12-22 19:30:20 -0600

    auto commit

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# update.sh 
+# updates the html files and pushes them to github
+
+# first git pulls any changes off of github (for updates to python scripts)
+
+git pull 
+
+# runs main.py 
+
+py main.py
+
+sleep 1
+
+# git push 
+
+git add .
+git commit -m "auto commit"
+if [ -n "$(git status - porcelain)" ];
+then
+    echo "No Changes"
+else
+    git status
+    echo "Pushing data..."
+    git push
+fi
+
+sleep 3
